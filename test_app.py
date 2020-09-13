b4619b468fdbba8fd105ef7b1bf1665235413b89
Royel <royelsoft@gmail.com>
2020-09-13 17:23:24 +0600

    hnd name pattern added and conditions changes

--- test_app.py
+++ test_app.py
@@ -1,5 +1,23 @@
 import sys
+
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('D:/Python Code/Short-Name-Generator/icon.png'))
+
+        self.show()
 
 def main():
 
@@ -8,7 +26,8 @@ def main():
     w = QWidget()
     w.resize(800, 600)
     w.move(400, 200)
-    w.setWindowTitle('Simple')
+    w.setWindowTitle('Short Name Generator')
+    e = Example()
     w.show()
 
     sys.exit(app.exec_())
